13ad980eb03ff5773801d2e4076210a997641d43
Your Name <Your Email>
2023-08-22 13:03:43 +0530

    [UPDATES]: UI changed, insert images with api, deleted css and images folder, code updates in js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,17 @@
-// require('intersection-observer')
-
-const options = {
-    lazyParentClass: '.lazy-load-item',
-    lazyItemClass: 'img' };
-  
-  
-  const lazyLoadParents = document.querySelectorAll(options.lazyParentClass);
-  
-  const lazyLoad = new IntersectionObserver(entries => {
-    entries.map(entry => {
-      // check if observed entry is intersecting
-      if (!entry.isIntersecting) return false;
-  
-      // target = intersected element
-      const img = entry.target.querySelector(options.lazyItemClass);
-  
-      if (img) {
-        if (img.dataset.srcset) {
-          // move data-srcset to srcset
-          img.srcset = img.dataset.srcset;
-          img.removeAttribute('data-srcset');
+/** First we get all the non-loaded image elements **/
+const lazyImages = document.querySelectorAll(".lazy-load");
+/** Then we set up a intersection observer watching over those images and whenever any of those becomes visible on the view then replace the placeholder image with actual one, remove the non-loaded class and then unobserve for that element **/
+const lazyImageObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy-load");
+            lazyImageObserver.unobserve(lazyImage);
         }
-        if (img.dataset.src) {
-          // move data-src to src
-          img.src = img.dataset.src;
-          img.removeAttribute('data-src');
-        }
-        // wait for image to load and addClass to fade in
-        img.onload = () => img.classList.add('loaded');
-      }
     });
-  });
-  
-  lazyLoadParents.forEach(item => {
-    // add items to IntersectionObserver
-    lazyLoad.observe(item);
-  });
+});
+/** Now observe all the non-loaded images using the observer we have setup above **/
+lazyImages.forEach(lazyImage => {
+    lazyImageObserver.observe(lazyImage);
+});
